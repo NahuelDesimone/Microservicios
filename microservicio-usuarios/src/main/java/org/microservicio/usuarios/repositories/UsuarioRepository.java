7b579e5016db199c02332c1a3c260952db5356c5
Nahuel Desimone <n.desimone@redlink.com.ar>
2024-09-18 16:35:48 -0300

    Se sube el proyecto correctamente

---
+++ microservicio-usuarios/src/main/java/org/microservicio/usuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package org.microservicio.usuarios.repositories;
+
+import org.microservicio.usuarios.models.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
+}
