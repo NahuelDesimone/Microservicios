03496ac53b214301b1c1bd66fb7db40f478104fb
Nahuel Desimone <n.desimone@redlink.com.ar>
2024-09-20 17:26:36 -0300

    Se crea de cero el nuevo microservicio de cursos y se agrega la coleccion de postman

---
+++ microservicio-cursos/src/main/java/org/microservicio/cursos/MicroservicioCursosApplication.java
@@ -0,0 +1,13 @@
+package org.microservicio.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCursosApplication.class, args);
+	}
+
+}
